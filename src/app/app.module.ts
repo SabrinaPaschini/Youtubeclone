a3b269154f8947787ddd28ee1cdbe4cb04024e23
Sabrina Paschini <sabrina.paschini@hotmail.com>
2024-06-09 18:58:58 -0300

    aside component done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SectionComponent } from './section/section.component';
+import { AsideComponent } from './aside/aside.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SectionComponent
+    SectionComponent,
+    AsideComponent
   ],
   imports: [
     BrowserModule,
